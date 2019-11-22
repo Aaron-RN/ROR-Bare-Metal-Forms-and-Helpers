6e497da9dd6324591b8980be32acf574a706def8
Aaron-RN <aaron.rory.newbold@gmail.com>
2019-11-22 18:00:44 -0500

    created form using form_for tag

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,24 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+  end
+
   def create
-    @user = User.new(username: params[:user_name], email: params[:user_email], password: params[:user_password])
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
 
     if @user.save
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
       redirect_to users_path
     else
       render :new
@@ -20,6 +34,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:user_name, :user_email, :user_password)
+    params.require(:user).permit(:username, :email, :password)
   end
 end
