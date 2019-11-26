9fbc61e1b2709b0ed8a8e67c1652afa57d77d658
Aaron Rory Newbold <aaron.rory.newbold@gmail.com>
2019-11-26 10:20:55 -0500

    Update users_controller.rb
    -Update method now renders: edit and redirects_to edit_users_path.
    -Create method now redirects_to new_users_path.
    -Removed commented code from Create method.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,12 +15,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    # @user = User.new(username: params[:username], email: params[:email],
-    #                  password: params[:password])
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to users_path
+      redirect_to new_users_path
     else
       render :new
     end
@@ -30,9 +28,9 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
 
     if @user.update(user_params)
-      redirect_to users_path
+      redirect_to edit_users_path
     else
-      render :new
+      render :edit
     end
   end
 
